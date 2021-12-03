cfc1fa19b9c5179148c589f05a892975128dd175
Yasuyuki Saito <ya_saito@uluru.jp>
2021-12-03 21:27:09 +0900

    テスト追加

---
+++ test/Tutorial.spec.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { shallowMount } from '@vue/test-utils'
+import Tutorial from '@/components/Tutorial.vue'
+
+describe('Tutorial', () => {
+  test('is a Vue instance', () => {
+    const wrapper = shallowMount(Tutorial)
+    expect(wrapper.vm).toBeTruthy()
+    expect(wrapper.vm.value).toBe('hoge')
+  })
+
+  test('method', () => {
+    const wrapper = shallowMount(Tutorial)
+    wrapper.vm.hoge()
+    expect(wrapper.vm.hoge).toBeTruthy()
+  })
+})
+</script>
