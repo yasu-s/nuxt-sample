2530d4d3e2e02483fffc4fb2c52cb7565d15b6af
Yasuyuki Saito <ya_saito@uluru.jp>
2021-12-04 20:13:50 +0900

    デバッグ設定

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,7 +1,7 @@
 <!-- Please remove this file from your project -->
 <template>
   <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
-    <h3>{{ hoge() }}</h3>
+    <h3 @click="hoge()">{{ value }}</h3>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet" />
     <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
       <a class="flex justify-center pt-8 sm:pt-0" href="https://nuxtjs.org" target="_blank">
@@ -98,7 +98,8 @@ export default Vue.extend({
   },
   methods: {
     hoge() {
-      console.log('')
+      const str = 'aaass'
+      console.log(str)
     },
   },
 })
--- nuxt.config.js
+++ nuxt.config.js
@@ -49,5 +49,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    extend(config, { isClient }) {
+      config.devtool = 'inline-cheap-module-source-map'
+    }
   }
 }
