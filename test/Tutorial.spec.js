46c557e75e58eb6d25537be4eed907861c8a3814
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2021-12-05 20:32:51 +0900

    簡略化

--- test/Tutorial.spec.js
+++ test/Tutorial.spec.js
@@ -2,12 +2,6 @@ import { shallowMount } from '@vue/test-utils'
 import Tutorial from '@/components/Tutorial.vue'
 
 describe('Tutorial', () => {
-  test('is a Vue instance', () => {
-    const wrapper = shallowMount(Tutorial)
-    expect(wrapper.vm).toBeTruthy()
-    expect(wrapper.vm.value).toBe('hoge')
-  })
-
   test('method', () => {
     const wrapper = shallowMount(Tutorial)
     console.log = jest.fn()
